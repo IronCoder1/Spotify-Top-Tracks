7c9da8ece36debac03e5e5d1af00f9bb5de385af
Anthony Olu <b.ioanth1000@gmail.com>
2016-05-18 16:35:19 +0100

    Initial Commit

---
+++ Spotify Top Tracks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Spotify Top Tracks
+//
+//  Created by MBPinTheAir on 18/05/2016.
+//  Copyright © 2016 moorsideinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Spotify Top Tracks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spotify Top Tracks
+//
+//  Created by MBPinTheAir on 18/05/2016.
+//  Copyright © 2016 moorsideinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
